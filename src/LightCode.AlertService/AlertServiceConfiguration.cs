1734c48c0831e9ec4bf57e999b96005c3c67b6e8
Nazareno Manco <nazareno.manco@lightcode.net>
2021-11-26 17:57:35 +0100

    aggiunto setup

---
+++ src/LightCode.AlertService/AlertServiceConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace LightCode.AlertService
+{
+    public static class AlertServiceConfiguration
+    {
+        public static void Setup(this IServiceCollection services, IConfiguration configuration, string configurationSectionKey = "AlertService")
+        {
+            services.AddScoped<IAlertService, AlertService>();
+            services.Configure<AlertServiceOptions>((o) => configuration.GetSection(configurationSectionKey));
+        }
+    }
+}
